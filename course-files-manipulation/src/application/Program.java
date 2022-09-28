8b9711b5691353f2f6d81bb7ac0db7e58cb794a4
Juliana-Ferreira <juliana_cristina.ferreira@hotmail.com>
2022-09-28 05:00:38 -0300

    Manipulação de arquivos no Java

---
+++ course-files-manipulation/src/application/Program.java
@@ -0,0 +1,35 @@
+package application;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter a folder path: ");
+		String folderPath = sc.nextLine();
+
+		File path = new File(folderPath);
+		
+		File[] folders = path.listFiles(File::isDirectory);
+		System.out.println("Folders:");
+		for(File folder : folders) {
+			System.out.println(folder);
+		}
+		
+		System.out.println();
+		File[] files = path.listFiles(File::isFile);
+		System.out.println("Files:");
+		for(File file : files) {
+			System.out.println(file);
+		}
+		
+		boolean success = new File(folderPath + "\\subpasta").mkdir();
+		System.out.println("Directory created successfully: " + success);
+		
+		sc.close();
+	}
+}
