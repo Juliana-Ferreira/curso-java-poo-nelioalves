850017d2776d0b0e126e96b17d2d9e82dc307a53
Juliana-Ferreira <juliana_cristina.ferreira@hotmail.com>
2022-10-31 15:16:04 -0300

    Busca no banco de dados (JPA/Hibernate)

--- aulajpamaven/src/main/java/aplicacao/Programa.java
+++ aulajpamaven/src/main/java/aplicacao/Programa.java
@@ -9,18 +9,14 @@ import dominio.Pessoa;
 public class Programa {
 
 	public static void main(String[] args) {
-
-		Pessoa p1 = new Pessoa(null, "Carla Nogueira", "carla.n@gmail.com");
-		Pessoa p2 = new Pessoa(null, "Lucas Oliveira", "lucas@gmail.com");
-		Pessoa p3 = new Pessoa(null, "Vanessa Camargo", "vanvan@gmail.com");
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
-		em.getTransaction().commit();
-		System.out.println("Done.");
+
+		Pessoa p = em.find(Pessoa.class, 2);
+		System.out.println(p);
+		
+		em.clear();
+		emf.close();
 	}
 }
